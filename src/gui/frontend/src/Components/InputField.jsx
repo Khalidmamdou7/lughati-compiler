f327b9eaffa3c940faa5637d2400797ab09a620d
Anas Sherif <96656458+Femton02@users.noreply.github.com>
2023-05-19 18:30:38 +0300

    Added the line numbering

--- src/gui/frontend/src/Components/InputField.jsx
+++ src/gui/frontend/src/Components/InputField.jsx
@@ -1,16 +1,44 @@
 import "./input_field.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const InputField = (props) => {
   const [textFieldValue, setTextFieldValue] = useState("");
-  
-  const InputChangeHandler=(e)=> {
+  const textareaRef = useRef(null);
+  const lineNumbersRef = useRef(null);
+
+  const InputChangeHandler = (e) => {
     props.onChange(e.target.value);
-  }
+  };
+
+  const handleTextareaScroll = () => {
+    lineNumbersRef.current.scrollTop = textareaRef.current.scrollTop;
+  };
+
+  const renderLineNumbers = () => {
+    return textFieldValue.split("\n").map((_, index) => (
+      <div key={index + 1} className="line-number">
+        {index + 1}
+      </div>
+    ));
+  };
 
   return (
-      <textarea rows="30" cols="40" className="textarea" onChange={InputChangeHandler}></textarea>
-  )
+    <div className="input-field">
+      <div className="line-numbers-column" ref={lineNumbersRef}>
+        {renderLineNumbers()}
+      </div>
+      <textarea
+        rows="30"
+        cols="40"
+        className="textarea"
+        value={textFieldValue}
+        onChange={InputChangeHandler}
+        onInput={(e) => setTextFieldValue(e.target.value)}
+        onScroll={handleTextareaScroll}
+        ref={textareaRef}
+      ></textarea>
+    </div>
+  );
 };
 
 export default InputField;
